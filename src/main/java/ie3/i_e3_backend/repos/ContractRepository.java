35bc81dc8934f1ff46ffc4c118b32108dd41e4b8
Lucas Murakami <lucaskaitomurakami@gmail.com>
2025-09-23 17:21:15 -0300

    FEATURE: Query to search active contract of employee by ID

--- src/main/java/ie3/i_e3_backend/repos/ContractRepository.java
+++ src/main/java/ie3/i_e3_backend/repos/ContractRepository.java
@@ -1,8 +1,14 @@
 package ie3.i_e3_backend.repos;
 
 import ie3.i_e3_backend.domain.Contract;
+
+import java.time.OffsetDateTime;
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 
 public interface ContractRepository extends JpaRepository<Contract, Long> {
@@ -11,4 +17,9 @@ public interface ContractRepository extends JpaRepository<Contract, Long> {
 
     List<Contract> findAllByProfileId(Long id);
 
+    @Query("SELECT c FROM Contract c " +
+            "WHERE c.users.id = :employeeId " +
+            "AND :now BETWEEN c.startDate AND c.endDate " +
+            "ORDER BY c.startDate DESC")
+    Optional<Contract> findTopActiveContractByEmployeeId(@Param("employeeId") Long employeeId, @Param("now") OffsetDateTime now);
 }
